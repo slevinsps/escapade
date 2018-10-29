101e1bce7fc49d097030e61b23eb9e7d54344baf
Ivan <spasenovivan@mail.ru>
2018-10-29 18:21:41 +0300

    add tests

---
+++ g_tests/battle_tank_test/tank_test.h
@@ -0,0 +1,4 @@
+#ifndef TANK_TEST_H
+#define TANK_TEST_H
+
+#endif // TANK_TEST_H
--- g_tests/battle_tank_test/tst_test.h
+++ g_tests/battle_tank_test/tst_test.h
@@ -1,10 +1,6 @@
+#pragma once
+
 #include <gtest/gtest.h>
 #include <gmock/gmock-matchers.h>
 
 using namespace testing;
-
-TEST(test, tests)
-{
-    EXPECT_EQ(1, 1);
-    ASSERT_THAT(0, Eq(0));
-}
